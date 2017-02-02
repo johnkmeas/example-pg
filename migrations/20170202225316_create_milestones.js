a0e0e6335377de93818ec9e0c6b1db00ef9ebe92
johnkmeas <johnkmeas@gmail.com>
2017-02-02 23:27:30 +0000

    redo of migrations

--- migrations/20170202225316_create_milestones.js
+++ migrations/20170202225316_create_milestones.js
@@ -1,9 +1,9 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('milestones', function(table){
-      table.increments('id');
       table.string('description');
-      table.string('password');
+      table.date('date_achieved');
+      table.increments('id')
     })
   ])
 };
